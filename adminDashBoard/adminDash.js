86fbcffe83a805c0e8b7b1d5567e38b66efe7808
AbhinavTyagi30 <abhinavtyagi30@gmail.com>
2024-02-22 22:06:25 +0530

    admin dashboard implemented

---
+++ adminDashBoard/adminDash.js
@@ -0,0 +1,16 @@
+let baseUrl = "http://ourhomeserver.onrender.com"
+
+
+
+//This function will take product data and append it to DOM as a card
+
+function appendProduct(data){
+    let product = document.getElementById("products-card");
+
+    let image = document.createElement("img");
+    image.setAttribute("src",`${data.image}`);
+    
+    let name = document.createElement("div");
+    dispatchEvent.setAttribute("class", )
+
+}
